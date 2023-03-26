1161965240d9cfb5ff92ce0425d932054e2b582a
Marcelo Pirro <marcelopirro98@gmail.com>
2023-03-26 00:44:51 -0300

    First Class: Position[C

---
+++ Chess/src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+    public static void main(String[] args) throws Exception {
+        Position pos = new Position(3, 4);
+        System.out.println(pos);
+    }
+    
+}
