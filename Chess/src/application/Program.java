a45b21f7d71fd768c5c1778decf2df4e99de42ef
Marcelo Pirro <marcelopirro98@gmail.com>
2023-03-26 00:54:00 -0300

    Starting to implement Board and Piece

--- Chess/src/application/Program.java
+++ Chess/src/application/Program.java
@@ -1,12 +1,13 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
     public static void main(String[] args) throws Exception {
-        Position pos = new Position(3, 4);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
+        
     }
     
 }
