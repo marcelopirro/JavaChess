a2c8510a75c58cf93e92d982fadc113e93051eab
Marcelo Pirro <marcelopirro98@gmail.com>
2023-03-26 21:17:49 -0300

    Check logic

--- Chess/src/application/UI.java
+++ Chess/src/application/UI.java
@@ -76,6 +76,10 @@ public class UI {
         System.out.println();
         System.out.println("Turn : " + chessMatch.getTurn());
         System.out.println("Waiting player : " + chessMatch.getCurrentPlayer());
+
+        if (chessMatch.getCheck()){
+            System.out.println("CHECK!");
+        }
     }
 
     public static void printBoard(ChessPiece[][] pieces){
@@ -107,12 +111,12 @@ public class UI {
         System.out.print("White: ");
         System.out.print(ANSI_WHITE);
         System.out.println(Arrays.toString(white.toArray()));
-        System.out.println(ANSI_RESET);
+        System.out.print(ANSI_RESET);
 
         System.out.print("Black: ");
         System.out.print(ANSI_YELLOW);
         System.out.println(Arrays.toString(black.toArray()));
-        System.out.println(ANSI_RESET);
+        System.out.print(ANSI_RESET);
 
     }
 
